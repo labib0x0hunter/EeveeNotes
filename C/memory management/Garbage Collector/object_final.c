d4b7db5b50a88601b5415677c7de153fc53bab31
labib0x0hunter <labibfaisal450@gmail.com>
2025-11-03 23:52:47 +0600

    updated object_final.c

--- C/memory management/Garbage Collector/object_final.c
+++ C/memory management/Garbage Collector/object_final.c
@@ -62,26 +62,136 @@ object_t* NewObjectFloat(float val) {
 	return obj;
 }
 
-object_t* NewObjectString(char* s) {}
-object_t* NewObjectArray(int size) {}
-object_t* NewObjectVector3(object_t* x, object_t* y, object_t* z) {}
+object_t* NewObjectString(char* val) {
+	object_t* obj = (object_t*) malloc(sizeof(object_t));
+	if (obj == NULL) {
+		return NULL;
+	}
+	obj->Kind = STRING;
+	obj->Data.v_string = val;	// is there any problem ?? we just copied the pointer to obj.
+	return obj;
+}
 
-bool set_array(object_t* obj, int index, object_t* val) {}	// obj[index] = val
-object_t* get_array(object_t* obj, int index) {}	// retrive obj[index]
-int len(object_t* obj) {}	// length of object
-object_t* add(object_t* a, object_t* b) {}	// merge two objects
+object_t* NewObjectArray(int size) {
+	object_t* obj = (object_t*) malloc(sizeof(object_t));
+	if (obj == NULL) {
+		return NULL;
+	}
+	obj->Kind = ARRAY;
+	obj->Data.v_array.len = size;
+	obj->Data.v_array.data = (object_t*) malloc(sizeof(object_t) * size);
+	if (obj->Data.v_array.data == NULL) {
+		free(obj);
+		return NULL;
+	}
+	return obj;
+}
 
+object_t* NewObjectVector3(object_t* x, object_t* y, object_t* z) {
+	if (x == NULL || y == NULL || z == NULL) {
+		return NULL;
+	}
+	object_t* obj = (object_t*) malloc(sizeof(object_t));
+	if (obj == NULL) {
+		return NULL;
+	}
+	obj->Kind = VECTOR3;
+	obj->Data.v_vector3.x = x;
+	obj->Data.v_vector3.y = y;
+	obj->Data.v_vector3.z = z;
+	return obj;
+}
 
-void PrintObject(object_t* obj) {
+// obj[index] = val
+bool set_array(object_t* obj, int index, object_t* val) {
+	if (obj == NULL) return false;
+	if (obj->Kind != ARRAY) return false;
+	if (index >= obj->Data.v_array.len) return false;
+	obj->Data.v_array.data[index] = val;
+	return true;
+}
+
+// retrive obj[index]
+object_t* get_array(object_t* obj, int index) {
+	if (obj == NULL) return NULL;
+	if (obj->Kind != ARRAY) return NULL;
+	if (index >= obj->Data.v_array.len) return NULL;
+	return obj->Data.v_array.data[index];
+}
+
+// length of object
+int len(object_t* obj) {
+	if (obj == NULL) return -1;
+	switch (obj->Kind) {
+		case INT:
+			return 1;
+		case FLOAT:
+			return 1;
+		case STRING:
+			return strlen(obj->Data.v_string);
+		case ARRAY:
+			return obj->Data.v_array.len;
+		case VECTOR3:
+			return 3;
+		default:
+			return -1;
+	}
+}
+
+// merge two objects of same kind except INT and FLOAT..
+object_t* add(object_t* a, object_t* b) {
+	if (a == NULL && b == NULL) return NULL;
+	// if (a->Kind != b->Kind) return NULL;
+
+	switch (a->Kind) {
+		case INT: {
+			switch (b->Kind) {
+				default:
+					return NULL;
+			}
+			break;
+		}
+		case FLOAT: {
+			switch (b->Kind) {
+				default:
+					return NULL;
+			}
+			break;
+		}
+		case STRING: {
+			if (b->Kind != STRING) return NULL;
+			break;
+		}
+		case ARRAY: {
+			if (b->Kind != ARRAY) return NULL;
+			break;
+		}
+		case VECTOR3: {
+			if (b->Kind != VECTOR3) return NULL;
+			break;
+		}
+		default:
+			return NULL;
+	}
+}
+
+// Print
+void PrintObject(object_t* obj, char delim) {
 	if (obj == NULL) {
 		return;
 	}
 	switch (obj->Kind) {
 		case INT:
-			printf("%d\n", obj->Data.v_int);
+			printf("%d%c", obj->Data.v_int, delim);
 			break;
 		case FLOAT:
-			printf("%0.3f\n", obj->Data.v_float);
+			printf("%0.3f%c", obj->Data.v_float, delim);
+			break;
+		case ARRAY:
+			for (int i = 0; i < obj->Data.v_array.len; i++) {
+				PrintObject(obj->Data.v_array.data[i], ' ');
+			}
+			printf("%c", delim);
 			break;
 		default:
 			printf("unknown data types\n");
@@ -94,8 +204,8 @@ int main() {
 	object_t* obj1 = NewObjectInt(10);
 	object_t* obj2 = NewObjectFloat(20.012);
 
-	PrintObject(obj1);
-	PrintObject(obj2);
+	PrintObject(obj1, '\n');
+	PrintObject(obj2, '\n');
 
 	if (obj1) free(obj1);
 	if (obj2) free(obj2);
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Programming Language Learning Notes
 
 # Resources
 - Stack frame and gdb : https://www.youtube.com/watch?v=7dMTCdFM2ss
+- Garbage collector : https://journal.stuffwithstuff.com/2013/12/08/babys-first-garbage-collector/
+- Memory Management in C : https://www.youtube.com/watch?v=rJrd2QMVbGM&t=30s
